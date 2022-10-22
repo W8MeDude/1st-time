1b940f7ec73a509b45af93b99fef17f871b0303a
W8MeDude <111272511+W8MeDude@users.noreply.github.com>
2022-10-22 19:30:18 +0300

    3

--- script.js
+++ script.js
@@ -49,3 +49,15 @@ myInput.onkeyup = function() {
         length.classList.add("invalid");
     }
 }
+
+
+
+function onChange() {
+    const password = document.querySelector('input[name=password]');
+    const confirm = document.querySelector('input[name=cpassword]');
+    if (confirm.value === password.value) {
+        confirm.setCustomValidity('');
+    } else {
+        confirm.setCustomValidity('Passwords do not match');
+    }
+}
